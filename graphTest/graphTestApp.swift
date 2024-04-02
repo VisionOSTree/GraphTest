96688ce54da4707c1ed3818380c636cb8f52fd50
Bocchi <oce.bocchi@gmail.com>
2024-04-02 16:23:47 +1100

    Initial Commit

---
+++ graphTest/graphTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  graphTestApp.swift
+//  graphTest
+//
+//  Created by Daniel Braithwaite on 2/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct graphTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
