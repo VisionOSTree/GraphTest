7759711318c59455dba9fd1c02cb5b041af7974b
Bocchi <oce.bocchi@gmail.com>
2024-04-11 14:12:15 +1000

    Implemented dynamic graph

--- graphTest/ContentView.swift
+++ graphTest/ContentView.swift
@@ -36,57 +36,26 @@ struct HeartRate: Identifiable {
 
 struct ContentView: View {
     
-    let hrVals = [
-        HeartRate(time: 0, rate: 120.786),
-        HeartRate(time: 1, rate: 119.487),
-        HeartRate(time: 2, rate: 119.683),
-        HeartRate(time: 3, rate: 118.828),
-        HeartRate(time: 4, rate: 117.978),
-        HeartRate(time: 5, rate: 116.65),
-        HeartRate(time: 6, rate: 115.356),
-        HeartRate(time: 7, rate: 114.682),
-        HeartRate(time: 8, rate: 114.677),
-        HeartRate(time: 9, rate: 113.291),
-        HeartRate(time: 10, rate: 112.379),
-        HeartRate(time: 11, rate: 111.437),
-        HeartRate(time: 12, rate: 110.706),
-        HeartRate(time: 13, rate: 108.767),
-        HeartRate(time: 14, rate: 108.714),
-        HeartRate(time: 15, rate: 107.585),
-        HeartRate(time: 16, rate: 106.455),
-        HeartRate(time: 17, rate: 105.276),
-        HeartRate(time: 18, rate: 103.819),
-        HeartRate(time: 19, rate: 102.924),
-        HeartRate(time: 20, rate: 101.215),
-        HeartRate(time: 21, rate: 99.276),
-        HeartRate(time: 22, rate: 99.659),
-        HeartRate(time: 23, rate: 98.403),
-        HeartRate(time: 24, rate: 96.867),
-        HeartRate(time: 25, rate: 96.37),
-        HeartRate(time: 26, rate: 94.985),
-        HeartRate(time: 27, rate: 94.18),
-        HeartRate(time: 28, rate: 92.472),
-        HeartRate(time: 29, rate: 91.167),
-        HeartRate(time: 30, rate: 90.073),
-        HeartRate(time: 31, rate: 88.067),
-        HeartRate(time: 32, rate: 88.459),
-        HeartRate(time: 33, rate: 87.006),
-        HeartRate(time: 34, rate: 85.576),
-        HeartRate(time: 35, rate: 84.588),
-        HeartRate(time: 36, rate: 83.303),
-        HeartRate(time: 37, rate: 81.679),
-        HeartRate(time: 38, rate: 81.177),
-        HeartRate(time: 39, rate: 79.803),
-        HeartRate(time: 40, rate: 78.531),
-        HeartRate(time: 41, rate: 77.197),
-        HeartRate(time: 42, rate: 75.678),
-        HeartRate(time: 43, rate: 74.393),
-        HeartRate(time: 44, rate: 72.57),
-        HeartRate(time: 45, rate: 71.748),
-        HeartRate(time: 46, rate: 70.211),
-        HeartRate(time: 47, rate: 68.53),
-        HeartRate(time: 48, rate: 67.63),
-        HeartRate(time: 49, rate: 65.897)
+    @State var count: Int = 1
+    @State var latest: Int = 1
+    @State var stop: Bool = true
+    
+    func callFunc() {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                hrVals.append(HeartRate(time: count, rate: Double.random(in: 60...120)))
+                if(hrVals.count > 20){
+                    hrVals.removeFirst()
+                }
+                print("got heart rate: \(count)")
+                count = count + 1
+                if !stop {
+                    callFunc()
+                }
+            }
+        }
+    
+    @State var hrVals = [
+        HeartRate(time: 0, rate: 120.786)
     ]
 
     
@@ -98,18 +67,35 @@ struct ContentView: View {
                 ForEach(hrVals){HeartRate in LineMark(x: .value("Time", HeartRate.time), y:.value("Heart Rate", HeartRate.rate) )
                 }
             }
+            .chartXScale(domain: count > 20 ? [count-20, count] : [0, 20])
 //            .chartPlotStyle { plotArea in
 //                plotArea
 //
 //            }
             .foregroundStyle(.red)
+            
+            
 
         }
+        Text("Your heart rate is: \(hrVals.index(before: count))")
+        Button(action: startStop, label: {
+            Text("stopped: \(stop)")
+        })
         
         
         .padding()
         
     }
+    
+    private func startStop(){
+        if stop { //Continue loop
+            stop = false
+            callFunc()
+        } else { //Stop loop
+            stop = true
+        }
+    }
+    
 }
 
 #Preview(windowStyle: .automatic) {
